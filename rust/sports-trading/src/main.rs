d7b12c2ba943a786b95c162bb1fa47685fb68380
Lewis Smith <ls.makesthings@gmail.com>
2022-07-24 18:01:14 +0100

    Adding Result Types/Data and improving State Machine Functionality

--- rust/sports-trading/src/main.rs
+++ rust/sports-trading/src/main.rs
@@ -1,6 +1,6 @@
 #[allow(dead_code)]
 #[derive(Debug)]
-enum EventState {
+enum State {
     INACTIVE = 0,
     PRE,
     INPLAY,
@@ -8,45 +8,71 @@ enum EventState {
     SETTLED
 }
 
-impl EventState {
-    fn open(&mut self) {
-        *self = EventState::PRE;
-    }
+#[allow(dead_code)]
+#[derive(Debug)]
+enum ResultType {
+    ORDER = 0,
+    SCORE,
+    LIST
+}
+
+#[allow(dead_code)]
+//#[derive(Debug)]
+type OrderResult = Vec<String>;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct ScoreResult(u32, u32);
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct ResultData {
+    order: OrderResult,
+    score: ScoreResult
+}
+
+impl ResultData {
+    fn new() -> ResultData {
+        ResultData {
+            order: OrderResult::new(),
+            score: ScoreResult(0,0)
+        }
+    }    
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct EventResult {
-    winners: Vec<String>,
-    placers: Vec<String>
+struct Result {
+    type_: ResultType,
+    data: ResultData
 }
 
-impl EventResult {
-    fn new() -> EventResult {
-        EventResult {
-            winners: Vec::new(),
-            placers: Vec::new()
+impl Result {
+    fn new() -> Result {
+        Result {
+            type_: ResultType::ORDER,
+            data: ResultData::new()
         }
     } 
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct EventContext {
+struct Context {
     competitors: Vec<String>,
     // markets: Vec<Markets>,
     // contracts: Vec<Contracts>,
-    result: EventResult,
+    result: Result,
     confirmed: bool
 }
 
-impl EventContext {
-    fn new() -> EventContext {
-        EventContext {
+impl Context {
+    fn new() -> Context {
+        Context {
             competitors: Vec::new(),
             // markets: Vec::new(),
             // contracts: Vec::new(),
-            result: EventResult::new(),
+            result: Result::new(),
             confirmed: false
         }
     }
@@ -54,23 +80,45 @@ impl EventContext {
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct EventMachine {
-    state: EventState,
-    context: EventContext
+struct Machine {
+    state: State,
+    context: Context
 }
 
-impl EventMachine {
-    fn new() -> EventMachine {
-        EventMachine {
-            state: EventState::INACTIVE,
-            context: EventContext::new()
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Events {
+    INIT=0,
+    OPEN,
+    START,
+    END,
+    SETTLE
+}
+
+impl Machine {
+    fn new() -> Machine {
+        Machine {
+            state: State::INACTIVE,
+            context: Context::new()
         }
+    }
+
+    fn send(&mut self, event: Events) {
+        println!("Recieved: {:?}", event);
+        match self.state {
+            State::INACTIVE => {
+                // TODO: Validate Initialisation
+            },
+            State::PRE => {},
+            State::INPLAY => {},
+            State::COMPLETE => {},
+            State::SETTLED => {}
+        } 
     }
 }
 
-fn main ()
-{
-    let mut event = EventMachine::new();
-    event.state.open();
+fn main () {
+    let mut event = Machine::new();
+    event.send(Events::OPEN);
     println!("{:?}", event);
 }
